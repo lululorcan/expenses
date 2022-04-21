f261170c5181a3b4c29c9b6de033ae33270ee1f5
Lorcan Travers <ltravers92@gmail.com>
2022-04-20 19:33:58 -0700

    add get cat dim function

--- src/functions/sw_funcs.py
+++ src/functions/sw_funcs.py
@@ -36,6 +36,35 @@ def sw_group_id(s, group_name):
           id = x.getId()
           return id
 
+def sw_get_category_dim(s):
+    """Create a Dataframe with all Splitwise Categories, with IDs"""
+    rows = []
+
+    cat = s.getCategories()
+
+    for x in cat:
+      subcat =  x.getSubcategories()
+      subcat_id = x.getId()
+      cat_name = x.getName()
+      cat_id = x.getId()
+      for y in subcat:
+        subcat_name = y.getName()
+        subcat_id = y.getId()
+        rows.append([cat_id, cat_name,subcat_id, subcat_name])
+
+    cat_df = pd.DataFrame(rows, columns=
+    ["cat_id", "cat_name","subcat_id","subcat_name"])
+
+    df_dtypes = {
+    "cat_id" : "int64",
+    "cat_name" : "category" ,
+    "subcat_id" : "int64",
+    "subcat_name" : "category"
+    }
+    #cat_df['Category'] = cat_df['Main Category'] + ': ' +  cat_df['SubCategory']
+    cat_df = cat_df.astype(df_dtypes)
+    return cat_df
+
 def sw_export_data(s,group_id,limit = 100000):
     export = s.getExpenses(group_id = group_id, limit=limit) #10000
     rows = []
@@ -46,8 +75,8 @@ def sw_export_data(s,group_id,limit = 100000):
         #expense info
         exp_id = e.getId()
         cat = e.getCategory()
-        subcat = cat.getSubcategories()
-        category_name = cat.getName()
+        subcat_id = cat.getId()
+        subcat_name = cat.getName()
         exp_desc = e.getDescription()
         # Other
         creation_method = e.getCreationMethod()
@@ -65,7 +94,7 @@ def sw_export_data(s,group_id,limit = 100000):
             owed_share = u.getOwedShare()
 
             rows.append(
-                [date, deleted_date, exp_id, category_name,exp_desc,
+                [date, deleted_date, exp_id, subcat_id,subcat_name,exp_desc,
                 creation_method, exp_cost ,exp_currency,user_id,
                  first_name,last_name,net_balance,paid_share,owed_share])
 
@@ -80,7 +109,7 @@ def sw_export_data(s,group_id,limit = 100000):
 
     # Clean Export
     export_df = pd.DataFrame(rows,
-    columns=["date", "deleted_date", "exp_id", "category_name","exp_desc",
+    columns=["date", "deleted_date", "exp_id", "subcat_id", "subcat_name","exp_desc",
     "creation_method", "exp_cost" ,"exp_currency","user_id",
      "first_name","last_name","net_balance","paid_share","owed_share"])
 
@@ -88,7 +117,8 @@ def sw_export_data(s,group_id,limit = 100000):
     "date" : "datetime64[ns]",
     "deleted_date" : "datetime64[ns]" ,
     "exp_id" : "int64",
-    "category_name" : "category",
+    "subcat_id" : "int64",
+    "subcat_name" : "category",
     "exp_desc" :  "object",
     "creation_method" : "object",
     "exp_cost" : "float",
@@ -101,12 +131,5 @@ def sw_export_data(s,group_id,limit = 100000):
      "owed_share" : "float"
      }
     export_df = export_df.astype(df_dtypes)
-
-    #export_df = export_df.loc[export_df['deleted_dt'].isnull()]
-    #export_df = export_df.drop(['creation_method', 'deleted_dt'], axis=1)
-    #Format Date
-    #export_df['date'] = export_df['date'].dt.date()
-    #export_df['date'] = export_df['date'].dt.strftime('%d/%m/%Y')
-    #export_df['deleted_date'] = pd.to_datetime(export_df.date)
-    #export_df['deleted_date'] = export_df['deleted_date'].dt.strftime('%d/%m/%Y')
+    ##TODO remove subcat_name as this can be derived from cat_dim table
     return export_df
