a264c179d585da546ac0ac2b05946c9fe27d351b
Lorcan Travers <ltravers92@gmail.com>
2022-04-20 19:32:59 -0700

    Add feature to enable Big Query to read string dtype without warning

--- src/functions/google_funcs.py
+++ src/functions/google_funcs.py
@@ -12,11 +12,11 @@ import pytz
 
 load_dotenv()
 # Connect to GSheets
-def decrypt_creds():
+def decrypt_creds(file_path):
     ENCRYPT_KEY= os.environ['ENCRYPT_KEY']
     #keys = ast.literal_eval(GOOGLE_JSON_KEY)
     f = Fernet(ENCRYPT_KEY)
-    with open("../encrypt_google_cloud_credentials.json", "rb") as file:
+    with open(file_path, "rb") as file:
         # read the encrypted data
         encrypted_data = file.read()
         # decrypt data
@@ -51,7 +51,7 @@ def big_query_connect(keys):
 
     return client
 
-def hello(client,table_id,dataframe):
+def big_query_load_spending(client,table_id,dataframe):
     # Example data
     #df = pd.DataFrame({'a': [1,2,4], 'b': ['123', '456', '000']})
 
@@ -64,25 +64,44 @@ def hello(client,table_id,dataframe):
     job_config = bigquery.LoadJobConfig(
     # Specify a (partial) schema. All columns are always written to the
     # table. The schema is used to assist in data type definitions.
-    schema=[
+    #schema=[
         # Specify the type of columns whose type cannot be auto-detected. For
         # example the "title" column uses pandas dtype "object", so its
         # data type is ambiguous.
-        bigquery.SchemaField("exp_desc", bigquery.enums.SqlTypeNames.STRING),
-        bigquery.SchemaField("category_name", bigquery.enums.SqlTypeNames.STRING),
-        bigquery.SchemaField("exp_currency", bigquery.enums.SqlTypeNames.STRING),
-        bigquery.SchemaField("first_name", bigquery.enums.SqlTypeNames.STRING),
-        bigquery.SchemaField("last_name", bigquery.enums.SqlTypeNames.STRING),
-        
+
+        #bigquery.SchemaField("exp_desc", bigquery.enums.SqlTypeNames.STRING),
+        #bigquery.SchemaField("category_name", bigquery.enums.SqlTypeNames.STRING),
+        #bigquery.SchemaField("exp_currency", bigquery.enums.SqlTypeNames.STRING),
+        #bigquery.SchemaField("first_name", bigquery.enums.SqlTypeNames.STRING),
+        #bigquery.SchemaField("last_name", bigquery.enums.SqlTypeNames.STRING),
+
         # Indexes are written if included in the schema by name.
 
-    ],
+    #],
     # Optionally, set the write disposition. BigQuery appends loaded rows
     # to an existing table by default, but with WRITE_TRUNCATE write
     # disposition it replaces the table with the loaded data.
     write_disposition="WRITE_TRUNCATE",
     )
+    # Change string columns
+    dataframe_dtype = pd.DataFrame(dataframe.dtypes)
 
+    string_list = ["object","category"]
+    filter_string_cols = []
+    for x in dataframe_dtype[0]:
+        if x in string_list:
+            y = True
+        else:
+            y = False
+        filter_string_cols.append(y)
+    string_columns = dataframe_dtype[filter_string_cols]
+    # Update schema for string columns only 
+    schema = []
+    for s in string_columns.index.tolist():
+        a = bigquery.SchemaField(s, bigquery.enums.SqlTypeNames.STRING)
+        schema.append(a)
+
+    job_config.schema = schema
     job = client.load_table_from_dataframe(
     dataframe, table_id, job_config=job_config
     )  # Make an API request.
