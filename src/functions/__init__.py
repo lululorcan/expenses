a0061d36a8b229da2487272ce3c5d9733a6d510c
Lorcan Travers <ltravers92@gmail.com>
2022-05-17 21:28:08 -0700

    remove unused module

--- src/functions/__init__.py
+++ src/functions/__init__.py
@@ -7,7 +7,6 @@ from splitwise import Splitwise
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
-from GetCategoryIDs import GetCategoryIDs
 import logging
 from cryptography.fernet import Fernet
 
